1df668a758f2c10b0f26cb84e69899ef92e3f4c9
Conner Crosby <conner@cavcrosby.tech>
2022-06-08 00:15:05 -0400

    Aggregate ansible common execs into var

--- ansible.mk
+++ ansible.mk
@@ -8,6 +8,11 @@ ANSIBLE_GALAXY = ansible-galaxy
 ANSIBLE_LINT = ansible-lint
 ANSIBLE_PLAYBOOK = ansible-playbook
 ANSIBLE_VAULT = ansible-vault
+ansible_executables = \
+	${ANSIBLE_GALAXY}\
+	${ANSIBLE_LINT}\
+	${ANSIBLE_PLAYBOOK}\
+	${ANSIBLE_VAULT}
 
 # sane defaults
 ANSIBLE_SRC = $(shell find . \( -type f \) \
